b6fffd4b38526a094ecc6fabaece97834dac27f7
Moh Bareche <mbareche.bic@gmail.com>
2019-09-21 21:19:43 -0400

    server-setup-client-final

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+const port = process.env.PORT || 8080;
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) console.log(`Could Not connect to database: `, err);
+    console.log(`Connecté avec succès à la base de données: ${config.db}`);
+})
+
+app.use(express.static(__dirname + '/client/dist/client/'));
+app.get('*', (req, res) => {
+    res.sendFile(path.joint(__dirname + '/client/dist/client/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+});
